2ea98368ebcdb8151c949ccafbf5dc177772f3aa
Jeffrey Cheng <2653399478@qq.com>
2018-03-02 14:18:52 +0800

    added pipeline block

--- vars/GlobalPipeline.groovy
+++ vars/GlobalPipeline.groovy
@@ -8,31 +8,36 @@ def call(body) {
 	node {
 		// Clean workspace before doing anything
 		deleteDir()
-
-		try {
-			stage ('Clone') {
-				checkout scm
-			}
-			stage ('Build') {
-				sh "echo 'building ${config.projectName} ...'"
-			}
-			stage ('Tests') {
-				parallel 'static': {
-					sh "echo 'shell scripts to run static tests...'"
-				},
-				'unit': {
-					sh "echo 'shell scripts to run unit tests...'"
-				},
-				'integration': {
-					sh "echo 'shell scripts to run integration tests...'"
-				}
-			}
-			stage ('Deploy') {
-				sh "echo 'deploying to server ${config.serverDomain}...'"
-			}
-		} catch (err) {
-			currentBuild.result = 'FAILED'
-			throw err
-		}
+	
 	}
+	
+	pipeline {
+		agent any
+		
+		parameters {
+			string (name: 'repoUrl', defaultValue: 'project_repoUrl', description: 'Project Repository URL')
+			string (name: 'repoBranch', defaultValue: 'project_repoBranch', description: 'Project Repository Branch')
+		}
+
+		stage ('Clone') {
+			checkout scm
+		}
+		stage ('Build') {
+			sh "echo 'building ${config.projectName} ...'"
+		}
+		stage ('Tests') {
+			parallel 'static': {
+				sh "echo 'shell scripts to run static tests...'"
+			},
+			'unit': {
+				sh "echo 'shell scripts to run unit tests...'"
+			},
+			'integration': {
+				sh "echo 'shell scripts to run integration tests...'"
+			}
+		}
+		stage ('Deploy') {
+			sh "echo 'deploying to server ${config.serverDomain}...'"
+		}
+
 }
