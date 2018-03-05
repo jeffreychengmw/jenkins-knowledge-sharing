bdf2563b5d1a0e1c79d1b1bce798e39b955b41b3
Jeffrey Cheng <2653399478@qq.com>
2018-03-05 17:34:48 +0800

    updated timeout

--- vars/GlobalPipeline.groovy
+++ vars/GlobalPipeline.groovy
@@ -57,22 +57,18 @@ def call(String type, Map map) {
 					steps {
 						//一些初始化操作
 						echo "Promote to Production Server"
-						
-
-						step {
-							timeout(time:45, unit: 'SECONDS') {
-								def userAccessToken = input(
-										id: 'promoteToProductionToken', message: 'Please input password to proceed',
-										parameters: [
-										             [$class: 'TextParameterDefinition', name: 'password']
-										            		 ]
-										)
-										//echo (userAccessToken['password'])
-										//echo (userAccessToken)
-										echo "User Access Token: ${userAccessToken}"
-										//echo (PASSWORD)
-							}
+						timeout(time:45, unit: 'SECONDS') {
+							def userAccessToken = input(
+								id: 'promoteToProductionToken', message: 'Please input password to proceed',
+								parameters: [
+								             [$class: 'TextParameterDefinition', name: 'password']
+								            		 ]
+							)
+							//echo (userAccessToken['password'])
+							//echo (userAccessToken)
+							//echo (PASSWORD)
 						}
+						echo "User Access Token: ${userAccessToken}"
 					}
 				}
 	        } // end stages
