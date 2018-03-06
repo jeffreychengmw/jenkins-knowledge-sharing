efe785f3527b751bd1cee3fd04b385ac7c4205df
Jeffrey Cheng <2653399478@qq.com>
2018-03-06 16:41:57 +0800

    added password

--- vars/DeployToProduction.groovy
+++ vars/DeployToProduction.groovy
@@ -5,6 +5,9 @@ def call() {
 	timeout(time:45, unit: 'SECONDS') {
 		def userAccessToken = input(
 			id: 'userAccessToken', message: 'Please input password to proceed',
+			parameters: [
+				[$class: 'TextParameterDefinition', name: 'password']
+			]
 		)
 		//echo "${CREDENTIALS}"
 	}
