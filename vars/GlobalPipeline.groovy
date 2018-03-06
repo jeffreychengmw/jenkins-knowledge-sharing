96d46467d1645a62f7a05013f42a2426b097680b
Jeffrey Cheng <2653399478@qq.com>
2018-03-06 10:29:10 +0800

    timeout fix

--- vars/GlobalPipeline.groovy
+++ vars/GlobalPipeline.groovy
@@ -4,11 +4,6 @@ def call(String type, Map map) {
 	        agent any
 	        //possible parameter types[booleanParam, choice, credentials, file, text, password, run, string]
 	        parameters {
-				string(name:'repoHost', defaultValue: "${map.repoHost}", description: 'repository branch')
-				string(name:'repoPath', defaultValue: "${map.repoPath}", description: 'repository path')
-				string(name:'repoBranch', defaultValue: "${map.repoBranch}", description: 'repository branch')
-				choice(name:'repoConnectionType', choices:"ssh\nhttp\nhttps\ngit\nsftp\nftp\nfile", description: 'please choose suitable connection type')
-				string(name:'repoConnectionPort', defaultValue: "${map.repoConnectionPort}", description: 'repository connection port')
 				string(name:'repoCredentials', defaultValue: "${map.repoCredentials}", description: 'repository credentials')
 			}
 	        //环境变量，初始确定后一般不需更改
@@ -18,8 +13,8 @@ def call(String type, Map map) {
 	        }
 	        //常量参数，初始确定后一般不需更改
 	        environment{
-	            REPO_URL="${map.repoConnectionType}://${map.repoHost}:${map.repoConnectionPort}${map.repoPath}"
-	        }
+
+			}
 	        options {
 	            disableConcurrentBuilds()
 	            timeout(time: 10, unit: 'MINUTES')
@@ -31,16 +26,14 @@ def call(String type, Map map) {
 	            stage('Build Initialization') {
 	                steps {
 	                //一些初始化操作
-	                    echo "${REPO_URL}"
-						echo "Repo Host: ${map.repoHost}"
-						echo "Repo Host: ${map.repoPath}"
+
 					}
 				}
 				stage('Fetch Codes from Repo') {
 					steps {
 					//一些初始化操作
 						script {
-							echo "starting fetchCode from ${REPO_URL}......"
+							echo "starting fetchCode from Git SCM......"
 							// Get some code from a GitHub repository
 							//git credentialsId:CRED_ID, url:REPO_URL, branch:params.repoBranch
 							checkout scm
@@ -59,7 +52,7 @@ def call(String type, Map map) {
 						echo "Promote to Production Server"
 						timeout(time:45, unit: 'SECONDS') {
 							def userAccessToken = input(
-								id: 'promoteToProductionToken', message: 'Please input password to proceed',
+								id: 'userAccessToken', message: 'Please input password to proceed',
 								parameters: [
 									[$class: 'TextParameterDefinition', name: 'password']
 								]
