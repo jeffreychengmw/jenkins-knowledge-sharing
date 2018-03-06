ee83505ed290970f50bbc3dbeda471484ffc7cfa
Jeffrey Cheng <2653399478@qq.com>
2018-03-06 15:29:25 +0800

    add new groovy class files

---
+++ vars/DeployToProduction.groovy
@@ -0,0 +1,15 @@
+def deployToProduction() {
+	timeout(time:45, unit: 'SECONDS') {
+		def userAccessToken = input(
+				id: 'userAccessToken', message: 'Please input password to proceed',
+				
+				)
+				//sh """
+				//echo ('User Access Token Password: '+userAccessToken['password'])
+				//echo ('User Access Token: '+userAccessToken)
+				//echo (userAccessToken['password'])
+				//echo "Env. Credentials: ${CREDENTIALS}"
+				//"""
+				//echo (PASSWORD)
+	}
+}
