ffdccbf9f87e541a9a21b91c1a100a69f4cabb30
Jeffrey Cheng <2653399478@qq.com>
2018-03-06 15:40:49 +0800

    echo

--- vars/DeployToProduction.groovy
+++ vars/DeployToProduction.groovy
@@ -1,15 +1,8 @@
 def call() { 
 	timeout(time:45, unit: 'SECONDS') {
 		def userAccessToken = input(
-				id: 'userAccessToken', message: 'Please input password to proceed',
-				
-				)
-				//sh """
-				//echo ('User Access Token Password: '+userAccessToken['password'])
-				//echo ('User Access Token: '+userAccessToken)
-				//echo (userAccessToken['password'])
-				//echo "Env. Credentials: ${CREDENTIALS}"
-				//"""
-				//echo (PASSWORD)
+			id: 'userAccessToken', message: 'Please input password to proceed',
+		)
+		echo "userAccessToken: ${userAccessToken}"
 	}
 }
