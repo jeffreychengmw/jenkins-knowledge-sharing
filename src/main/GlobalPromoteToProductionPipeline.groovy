e754cf812d588182eeac43bb635f6aedb339c959
Jeffrey Cheng <2653399478@qq.com>
2018-03-08 15:07:22 +0800

    pipeline

--- src/main/GlobalPromoteToProductionPipeline.groovy
+++ src/main/GlobalPromoteToProductionPipeline.groovy
@@ -1,22 +1,13 @@
 def call(String result) {
-		stage('CICD') {
-			sh """
-				pwd
-			"""
-			echo "stage CICD >>>>>>>>>>>>>>>>>>>>>>>>"
-		}
-	
-/*	if (result == "SUCCESS") {
-		pipeline {
+	pipeline {
+		stages {
 			stage('CICD') {
 				sh """
-					pwd
-					rm -rf /usr/local/cicd/war/home.war
-					cp target/*.war /usr/local/cicd/war/.
+				pwd
+				rm -rf /usr/local/cicd/war/home.war
+				cp target/*.war /usr/local/cicd/war/.
 				"""
 			}
 		}
-	} else {
-		echo 'result: >>>>> '+result
-	}*/
+	}
 }
