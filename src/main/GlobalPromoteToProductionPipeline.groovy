a5d84934d954a7ced0ec7844d1010ca63ee5fb3c
Jeffrey Cheng <2653399478@qq.com>
2018-03-08 13:59:07 +0800

    cicd

--- src/main/GlobalPromoteToProductionPipeline.groovy
+++ src/main/GlobalPromoteToProductionPipeline.groovy
@@ -1,3 +1,15 @@
 def call(String result) {
-	echo result
+	if (result == "SUCCESS") {
+		pipeline {
+			stage('CICD') {
+				sh """
+					pwd
+					rm -rf /usr/local/cicd/war/home.war
+					cp target/*.war /usr/local/cicd/war/.
+				"""
+			}
+		}
+	} else {
+		echo result
+	}
 }
